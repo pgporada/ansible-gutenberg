3fe9f1fa9954cc91f137f27a629214eca4a04304
Phil Porada <philporada@gmail.com>
2017-02-12 21:17:32 -0600

    Concurrency

--- proofreader.sh
+++ proofreader.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 # AUTHOR: Phil Porada
 
+CPU_COUNT=(( $(grep "^processor" /proc/cpuinfo | awk '{print $3}' | wc -l) / 2 + 1 ))
+
 if [ ! -f Gemfile.lock ]; then
     bundle install
 else
     bundle update
 fi
 
-bundle exec kitchen create
+bundle exec kitchen create -c $CPU_COUNT
 
 if [ $? -ne 0 ]; then
-    bundle exec kitchen create
+    bundle exec kitchen create -c $CPU_COUNT
 fi
 
 bundle exec kitchen list
 
-bundle exec kitchen converge
+bundle exec kitchen converge -c $CPU_COUNT
