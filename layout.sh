d72306ce71f97e6ebc26c4d667e110179f9d88ef
Phil Porada <philporada@gmail.com>
2017-02-18 22:59:17 -0600

    Automagic woot

--- layout.sh
+++ layout.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # AUTHOR: Phil Porada - philporada@gmail.com
 
-docker run -d -p 9200:9200 elasticsearch
-
-LOGSTASH_CID=$(docker run -d -p 5000:5000 --rm logstash -e 'input {tcp {port => 5000 codec => json}} output {elasticsearch { hosts => ["localhost:9200"] ssl_certificate_verification => false}}')
+ELASTICSEARCH_CID=$(docker run -d -p 9200:9200 elasticsearch)
+ELASTICSEARCH_IPADDR=$(docker inspect $ELASTICSEARCH_CID | grep IPAddress | tail -n1 | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g')
+LOGSTASH_CID=$(docker run -d -p 5000:5000 --rm logstash -e "input {tcp {port => 5000 codec => json}} output {elasticsearch { hosts => [\"$ELASTICSEARCH_IPADDR:9200\"] ssl_certificate_verification => false}}")
 
 docker logs -f $LOGSTASH_CID
