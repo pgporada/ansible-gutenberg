62c5bc5f77486ebc16f8dfd0a3936d884e28a4b6
Phil Porada <philporada@gmail.com>
2017-02-22 00:19:49 -0500

    Prints us template playbooks and commands to run said playbooks

--- printer.sh
+++ printer.sh
@@ -2,4 +2,27 @@
 # AUTHOR: Phil Porada - philporada@gmail.com
 # WHAT: Prints out playbook after playbook
 
-ansible-galaxy search ntp | awk 'NR>5' | awk '{print $1}' | sed 's/[[:space:]]//g'
+declare -a NTP_ROLES
+NTP_ROLES=( $(ansible-galaxy search ntp | awk 'NR>5' | awk '{print $1}' | sed 's/[[:space:]]//g') )
+
+mkdir -p playbooks/{ntp}
+
+for ROLE in ${NTP_ROLES[@]}; do
+    mkdir -p playbooks/ntp/$ROLE
+    cat << REQ > playbooks/ntp/$ROLE/requirements.yml
+---
+- src: $ROLE
+...
+REQ
+
+    cat << PLAYBOOK > playbooks/ntp/$ROLE/playbook_ntp_$ROLE.yml
+---
+- name: Installs and configures an ntp daemon with a role from Ansible Galaxy
+  hosts: all
+  roles:
+    - $ROLE
+...
+PLAYBOOK
+
+echo "PLAYBOOK=playbooks/ntp/$ROLE/playbook_ntp_$ROLE.yml REQUIREMENTS_PATH=playbooks/ntp/$ROLE/requirements.yml bundle exec kitchen converge"
+done
