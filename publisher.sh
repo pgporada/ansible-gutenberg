8d0a4f24cf48f517c97c9542b5625a8ee44796c0
Phil Porada <philporada@gmail.com>
2017-02-22 00:06:11 -0500

    Renamed these files

--- publisher.sh
+++ publisher.sh
@@ -1,89 +1,27 @@
 #!/bin/bash
 # AUTHOR: Phil Porada - philporada@gmail.com
+echo "+) Clearing old logs"
+rm -f elasticsearch-logs/*.json*
 
-# 1) Get all logstash-2017.02.14.data.json files
-echo "+-----------+"
-echo "+ Log Files +"
-echo "+-----------+"
-cd elasticsearch-logs
-LOGFILES=$(find . -type f -name "logstash-*.data.json" | sed 's|^./||')
-for i in ${LOGFILES}; do
-    echo ${i}
+echo "+) Getting elasticsearch information"
+ELASTICSEARCH_CID=$(docker ps | grep elasticsearch | awk '{print $1}')
+ELASTICSEARCH_SERVER=$(docker inspect $ELASTICSEARCH_CID | grep IPAddress | tail -n1 | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g')
+
+echo "+) Running elasticdump over the elasticsearch indices"
+export NODE_TLS_REJECT_UNAUTHORIZED=0
+INDICES=$(curl -skXGET http://${ELASTICSEARCH_SERVER}:9200/_cat/indices?pretty | cut -d $' ' -f3)
+for INDEX in $INDICES; do
+    echo $INDEX - mapping
+    elasticdump \
+        --bulk \
+        --type=mapping \
+        --input=http://${ELASTICSEARCH_SERVER}:9200/${INDEX} \
+        --output=elasticsearch-logs/${INDEX}.mapping.json
+
+    echo $INDEX - data
+    elasticdump \
+        --bulk \
+        --type=data \
+        --input=http://${ELASTICSEARCH_SERVER}:9200/${INDEX} \
+        --output=elasticsearch-logs/${INDEX}.data.json
 done
-echo
-# #-------
-# logstash-2017.02.14.data.json
-# logstash-2017.02.15.data.json
-# #-------
-
-# 2) Get all session IDs in all logstash data files. A single instance of a SID is a playbook run.
-echo "+-------------+"
-echo "+ Session IDs +"
-echo "+-------------+"
-declare -a SIDS
-SIDS=( $(for i in $LOGFILES; do jq -r '._source.session' $i; done 2>&1 | sed '/parse error/d' | sort -u) )
-for i in ${SIDS[@]}; do
-    echo ${i}
-done
-echo
-# #-------
-# ab28360e-f27b-11e6-b8cx-0242ac110004
-# ff0b955e-f27b-11e6-822x-0242ac110004
-# #-------
-
-# 3) Count the # of SIDS
-echo "+------------------+"
-echo "+ # of Session IDs +"
-echo "+------------------+"
-echo ${#SIDS[@]}
-echo
-# #-------
-# 256
-# #-------
-
-# 4) Get number of steps per SID
-echo "+---------------------------+"
-echo "+ # of steps per Session ID +"
-echo "+---------------------------+"
-# declare -A SIDCOUNT
-# for FILE in ${LOGFILES}; do \
-#     for SID in ${SIDS[@]}; do \
-#         jq -r ". | select(._source.session==\"${SID}\") | ._source.session" ${FILE} 2>&1 | sed '/parse error/d'; \
-#     done; \
-# done; \
-# sed '/^$/d' | sort -u | wc -l
-echo
-# #-------
-# ff0b955e-f27b-11e6-822x-0242ac110004: 7 tasks
-# bnkw4kxe-vsdk-23e6-482x-0242ac110004: 1 task
-# #-------
-
-
-# 5) Get names of all the playbooks
-echo "+---------------------------------+"
-echo "+ # Display name of playbooks ran +"
-echo "+---------------------------------+"
-LOGFILES=$(find . -type f -name "logstash-*.data.json" | sed 's|^./||')
-declare -a PLAYBOOKS
-PLAYBOOKS=( $(for i in $LOGFILES; do jq -r '._source.ansible_playbook' $i; done 2>&1 | sed '/parse error/d') )
-for i in ${PLAYBOOKS[@]}; do
-    echo ${i}
-done
-echo
-# #-------
-#
-# #-------
-
-# 6) Get names of all the roles from each session id
-echo "+----------------------------------+"
-echo "+ # Display names of all roles run +"
-echo "+----------------------------------+"
-for SID in ${SIDS[@]}; do
-    jq -r ". | select(._source.session==\"$SID\") | ._source.ansible_task" *.data.json
-done
-echo
-# #-------
-# TASK: setup
-# TASK: pgporada.repo-epel : Install EPEL repo
-# TASK: pgporada.repo-epel : Second pass to ensure EPEL is the latest version
-# #-------
---
+++ reviewer.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# AUTHOR: Phil Porada - philporada@gmail.com
+
+# 1) Get all logstash-2017.02.14.data.json files
+echo "+-----------+"
+echo "+ Log Files +"
+echo "+-----------+"
+cd elasticsearch-logs
+LOGFILES=$(find . -type f -name "logstash-*.data.json" | sed 's|^./||')
+for i in ${LOGFILES}; do
+    echo ${i}
+done
+echo
+# #-------
+# logstash-2017.02.14.data.json
+# logstash-2017.02.15.data.json
+# #-------
+
+# 2) Get all session IDs in all logstash data files. A single instance of a SID is a playbook run.
+echo "+-------------+"
+echo "+ Session IDs +"
+echo "+-------------+"
+declare -a SIDS
+SIDS=( $(for i in $LOGFILES; do jq -r '._source.session' $i; done 2>&1 | sed '/parse error/d' | sort -u) )
+for i in ${SIDS[@]}; do
+    echo ${i}
+done
+echo
+# #-------
+# ab28360e-f27b-11e6-b8cx-0242ac110004
+# ff0b955e-f27b-11e6-822x-0242ac110004
+# #-------
+
+# 3) Count the # of SIDS
+echo "+------------------+"
+echo "+ # of Session IDs +"
+echo "+------------------+"
+echo ${#SIDS[@]}
+echo
+# #-------
+# 256
+# #-------
+
+# 4) Get number of steps per SID
+echo "+---------------------------+"
+echo "+ # of steps per Session ID +"
+echo "+---------------------------+"
+# declare -A SIDCOUNT
+# for FILE in ${LOGFILES}; do \
+#     for SID in ${SIDS[@]}; do \
+#         jq -r ". | select(._source.session==\"${SID}\") | ._source.session" ${FILE} 2>&1 | sed '/parse error/d'; \
+#     done; \
+# done; \
+# sed '/^$/d' | sort -u | wc -l
+echo
+# #-------
+# ff0b955e-f27b-11e6-822x-0242ac110004: 7 tasks
+# bnkw4kxe-vsdk-23e6-482x-0242ac110004: 1 task
+# #-------
+
+
+# 5) Get names of all the playbooks
+echo "+---------------------------------+"
+echo "+ # Display name of playbooks ran +"
+echo "+---------------------------------+"
+LOGFILES=$(find . -type f -name "logstash-*.data.json" | sed 's|^./||')
+declare -a PLAYBOOKS
+PLAYBOOKS=( $(for i in $LOGFILES; do jq -r '._source.ansible_playbook' $i; done 2>&1 | sed '/parse error/d') )
+for i in ${PLAYBOOKS[@]}; do
+    echo ${i}
+done
+echo
+# #-------
+#
+# #-------
+
+# 6) Get names of all the roles from each session id
+echo "+----------------------------------+"
+echo "+ # Display names of all roles run +"
+echo "+----------------------------------+"
+for SID in ${SIDS[@]}; do
+    jq -r ". | select(._source.session==\"$SID\") | ._source.ansible_task" *.data.json
+done
+echo
+# #-------
+# TASK: setup
+# TASK: pgporada.repo-epel : Install EPEL repo
+# TASK: pgporada.repo-epel : Second pass to ensure EPEL is the latest version
+# #-------
