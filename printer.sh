479b6ad25868f014349970043ce19c1bf854e738
Phil Porada <philporada@gmail.com>
2017-02-22 00:33:33 -0500

    Printing each playbook 'chapter' into a script

--- printer.sh
+++ printer.sh
@@ -2,10 +2,14 @@
 # AUTHOR: Phil Porada - philporada@gmail.com
 # WHAT: Prints out playbook after playbook
 
+CPU_COUNT=$(( $(grep "^processor" /proc/cpuinfo | awk '{print $3}' | wc -l) + 2 ))
+
 declare -a NTP_ROLES
 NTP_ROLES=( $(ansible-galaxy search ntp | awk 'NR>5' | awk '{print $1}' | sed 's/[[:space:]]//g') )
 
 mkdir -p playbooks/{ntp}
+
+rm -f chapters.txt
 
 for ROLE in ${NTP_ROLES[@]}; do
     mkdir -p playbooks/ntp/$ROLE
@@ -24,5 +28,5 @@ REQ
 ...
 PLAYBOOK
 
-echo "PLAYBOOK=playbooks/ntp/$ROLE/playbook_ntp_$ROLE.yml REQUIREMENTS_PATH=playbooks/ntp/$ROLE/requirements.yml bundle exec kitchen converge"
+echo "PLAYBOOK=playbooks/ntp/$ROLE/playbook_ntp_$ROLE.yml REQUIREMENTS_PATH=playbooks/ntp/$ROLE/requirements.yml bundle exec kitchen converge -c $CPU_COUNT" >> chapters.txt
 done
