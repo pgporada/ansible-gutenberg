6e1f5fbbe3944de723496d95d6cf88b9ef0f2615
Phil Porada <philporada@gmail.com>
2017-02-22 23:53:18 -0500

    Need to have an arg for this script

--- writer.sh
+++ writer.sh
@@ -2,10 +2,15 @@
 # AUTHOR: Phil Porada - philporada@gmail.com
 # WHAT: Runs all of the commands inside chapters.txt. Outputs status
 #       to a separate file named status.log that you can tail.
+if [ $# -ne 1]; then
+    echo "Need an argument"
+    exit 1
+fi
+
 rm -f status.log
 
 while read LINE; do
     echo "[START]  - $(date) - ${LINE}" >> status.log
     eval "${LINE}"
     echo "[FINISH] - $(date) - ${LINE}" >> status.log
-done < chapters.txt
+done < $1
