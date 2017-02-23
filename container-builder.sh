3f813a0a0a0712f46e493adb5f18c5190102b335
Phil Porada <philporada@gmail.com>
2017-02-23 00:21:54 -0500

    Crappy conversion of the .kitchen.yml to dockerfiles because vim copy/paste is super fast

--- container-builder.sh
+++ container-builder.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # AUTHOR: Phil Porada - philporada@gmail.com
-# WHAT: Builds all of our testing containers
+# WHAT: Builds and tags all of our testing containers
 
-
+cd containers
+for i in $(find -type d | sed -e 's|./||g' -e 's/\.//g' -e '/^$/d'); do
+    cd $i
+    docker build -t c6h12o6-$i .
+    cd ..
+done
