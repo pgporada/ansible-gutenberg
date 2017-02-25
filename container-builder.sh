9a84d9541638b6a8ba556d1df87114ebb6b6dbf1
Phil Porada <philporada@gmail.com>
2017-02-25 00:05:31 -0500

    Added color to the output for clarity

--- container-builder.sh
+++ container-builder.sh
@@ -2,8 +2,12 @@
 # AUTHOR: Phil Porada - philporada@gmail.com
 # WHAT: Builds and tags all of our testing containers
 
+BLD=$(tput bold)
+RST=$(tput sgr0)
+
 cd containers
 for i in $(find -type d | sed -e 's|./||g' -e 's/\.//g' -e '/^$/d'); do
+    echo "${BLD}${i}${RST}"
     cd $i
     docker build -t c6h12o6-$i .
     cd ..
