e729fa86c77dc2270d7c8a3dbb1b8cae252d47e9
Phil Porada <philporada@gmail.com>
2017-03-02 22:25:14 -0500

    Why do I do these things in bash

--- printer.sh
+++ printer.sh
@@ -11,26 +11,26 @@ function usage {
     "
 }
 
-function printer {
+function print_docker {
     local CPU_COUNT=$(( $(grep "^processor" /proc/cpuinfo | awk '{print $3}' | wc -l) + 2 ))
-    local TYPE="${1}"
+    local ROLE="${1}"
     local DESC="${2}"
     declare -a ROLES
 
-    rm -f chapter_$TYPE.txt
-    ROLES=( $(ansible-galaxy search $TYPE | awk 'NR>5' | awk '{print $1}' | sed 's/[[:space:]]//g') )
+    rm -f chapter_$ROLE.txt
+    ROLES=( $(ansible-galaxy search $ROLE | awk 'NR>5' | awk '{print $1}' | sed 's/[[:space:]]//g') )
 
     for ROLE in ${ROLES[@]}; do
-        mkdir -p playbooks/$TYPE/$ROLE
+        mkdir -p playbooks/$ROLE/$ROLE
         # We already built the containers, let's blow them away and start them up immediately
-        echo "bundle exec kitchen destroy -c10; PLAYBOOK=playbooks/$TYPE/$ROLE/playbook_$TYPE_$ROLE.yml REQUIREMENTS_PATH=playbooks/$TYPE/$ROLE/requirements.yml bundle exec kitchen converge -c $CPU_COUNT; bundle exec kitchen create -c10;" >> chapter_$TYPE.txt
-        cat << REQ > playbooks/$TYPE/$ROLE/requirements.yml
+        echo "bundle exec kitchen destroy -c10; PLAYBOOK=playbooks/$ROLE/$ROLE/playbook_$ROLE_$ROLE.yml REQUIREMENTS_PATH=playbooks/$ROLE/$ROLE/requirements.yml bundle exec kitchen converge -c $CPU_COUNT; bundle exec kitchen create -c10;" >> chapter_$ROLE.txt
+        cat << REQ > playbooks/$ROLE/$ROLE/requirements.yml
 ---
 - src: $ROLE
 ...
 REQ
 
-        cat << PLAYBOOK > playbooks/$TYPE/$ROLE/playbook_$TYPE_$ROLE.yml
+        cat << PLAYBOOK > playbooks/$ROLE/$ROLE/playbook_$ROLE_$ROLE.yml
 ---
 - name: "${DESC}"
   hosts: all
@@ -41,11 +41,32 @@ PLAYBOOK
     done
 }
 
-if [ $# -ne 2 ]; then
-    usage
-    exit 1
-else
-    TYPE="${1}"
-    DESC="${2}"
-    printer "${TYPE}" "${DESC}"
-fi
+function print_ec2 {
+    echo hi
+}
+
+while getopts ":t:r:d:" opt; do
+  case $opt in
+    t) TYPE="${OPTARG}" ;;
+    r) ROLE="${OPTARG}" ;;
+    d) DESC="${OPTARG}" ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+for i in TYPE ROLE DESC; do
+    if [ -z ${!i} ]; then
+        usage
+        echo $i was not set
+        exit 1
+    fi
+done
+
+printer "${ROLE}" "${DESC}"
